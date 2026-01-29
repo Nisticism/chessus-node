d3fe6c3bef5725a91e7a73814642d7a5d0ca6460
hfoster00 <94389711+hfoster00@users.noreply.github.com>
2026-01-29 02:04:23 -0600

    Fix 413 error for piece image uploads by increasing body parser limit to 10MB

--- server/index.js
+++ server/index.js
@@ -79,7 +79,8 @@ app.use((req, res, next) => {
 const db_pool = require("../configs/db");
 const dbHelpers = require("./db-helpers");
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Serve static files from uploads directory with CORS headers
 app.use('/uploads', (req, res, next) => {
