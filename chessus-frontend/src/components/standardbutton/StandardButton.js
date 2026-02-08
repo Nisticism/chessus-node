40bddc459198769e2860cf00d1a5a1589a1d85b4
hfoster00 <94389711+hfoster00@users.noreply.github.com>
2026-02-08 02:40:58 -0600

    Add disabled, className, and children props to StandardButton

--- chessus-frontend/src/components/standardbutton/StandardButton.js
+++ chessus-frontend/src/components/standardbutton/StandardButton.js
@@ -1,8 +1,15 @@
 import React from "react";
 import styles from "./standard-button.module.scss";
-function StandardButton ({buttonText, onClick, buttonType}) {
+function StandardButton ({buttonText, onClick, buttonType, disabled, className, children}) {
   return (
-    <button className={styles["standard-button"]} onClick={onClick} type={buttonType ? buttonType : "button"}>{buttonText}</button>
+    <button 
+      className={`${styles["standard-button"]} ${className || ''}`} 
+      onClick={onClick} 
+      type={buttonType ? buttonType : "button"}
+      disabled={disabled}
+    >
+      {children || buttonText}
+    </button>
   );
 };
 
